42492b6b107f441f2895e5a06b3fb9f8ed87a465
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-17 12:39:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -160,3 +160,27 @@ while repetir:
                     ax = graficarComp([Vr])
                     ax.set_title("Vectores")
                     plt.show()
+               else:
+                    "OK :)"
+                rep=input("\033[1;32m"+"¿Desea graficar mas vectores? [S/N]")
+                if rep == "s" or rep == "S":
+                    repetir = True
+                else:
+                    repetir=False
+            elif v == 4:
+                print("\033[1;32m"+"Ingresa la coordenada en el eje x del vector : 1")
+                a=int(input())
+                print("\033[1;32m"+"Ingresa la coordenada en el eje y del vector : 1")
+                b=int(input())
+                infovector=ParOrdenado(a,b)
+                print("\033[1;32m"+"Ingresa la coordenada en el eje x del vector : 2")
+                a2=int(input())
+                print("\033[1;32m"+"Ingresa la coordenada en el eje y del vector : 2")
+                b2=int(input())
+                infovector2=ParOrdenado(a2,b2)
+                print("\033[1;32m"+"Ingresa la coordenada en el eje x del vector : 3")
+                a3=int(input())
+                print("\033[1;32m"+"Ingresa la coordenada en el eje y del vector : 3")
+                b3=int(input())
+                infovector3=ParOrdenado(a3,b3)
+                print("\033[1;32m"+"Ingresa la coordenada en el eje x del vector : 4"
