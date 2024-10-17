517dacd4d72866193de020ecc521e209c2c8e6d5
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-17 12:35:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 # Fecha: 17 octubre 2024
 # Descripción: Calculadora Vectorial en Python Version 2.
 # GitHub: https://github.com/Jona163
+
+from cmath import acos, asin, cos, pi, sqrt, tan
+import fractions
+from re import U
+import math
+from unittest import result
+from xml.etree.ElementTree import tostring
+from numpy import arctan
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from fractions import Fraction                          
+from decimal import Decimal
